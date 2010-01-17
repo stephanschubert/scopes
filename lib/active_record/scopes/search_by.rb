a3218724dd18de58919cc96500dca21d2ec66ca3
jazen <stephan@frozencherry.de>
2010-01-17 16:10:13 +0100

    Added new scopes to search by

---
+++ lib/active_record/scopes/search_by.rb
@@ -0,0 +1,45 @@
+module ActiveRecord
+  module Scopes
+    module SearchBy
+
+      SCOPES = {
+        :contains                 => [ "LIKE",     "%", "%" ],
+        :does_not_contain         => [ "NOT LIKE", "%", "%" ],
+        :starts_with              => [ "LIKE",      "", "%" ],
+        :does_not_start_with      => [ "NOT LIKE",  "", "%" ],
+        :ends_with                => [ "LIKE",     "%",  "" ],
+        :does_not_end_with        => [ "NOT LIKE", "%",  "" ],
+        :is                       => [ "=",         "",  "" ],
+        :is_not                   => [ "<>",        "",  "" ],
+        :is_less_than             => [ "<",         "",  "" ],
+        :is_less_than_or_equal    => [ "<=",        "",  "" ],
+        :is_greater_than          => [ ">"          "",  "" ],
+        :is_greater_than_or_equal => [ ">=",        "",  "" ],
+      }
+      
+      SCOPES.each do |name, (operator, prefix, suffix)|
+        named_scope name, lambda { |column, value|
+          { :conditions => [
+            "LOWER(#{column}) #{operator} ?", 
+            "#{prefix}#{value.is_a?(String) ? value.downcase : value}#{suffix}" 
+            ]
+          }
+        }
+      end
+      
+      def self.search_by_criteria(column, operator, value)
+        scope = operator.to_s.gsub(" ", "_").to_sym
+        send(scope, column, value)
+      end
+      
+      def self.search_by(*all_criteria)
+        scope = scoped({})
+        all_criteria.each do |c|
+          scope = scope.scoped(search_by_criteria(*c).proxy_options)
+        end
+        scope
+      end
+  
+    end
+  end
+end
